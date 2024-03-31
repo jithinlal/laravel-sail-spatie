fb2e535418c7929a777445c29c715ebe91e1ad2e
Jithinlal <jithtitan007@gmail.com>
2024-03-31 17:19:14 +0530

    feat: user create and update (#1)

--- resources/js/Pages/Users/Create.jsx
+++ resources/js/Pages/Users/Create.jsx
@@ -3,7 +3,7 @@ import {Head, useForm} from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
 import PrimaryButton from "@/Components/PrimaryButton.jsx";
 import {toast} from "react-hot-toast";
-import {Combobox, Transition} from '@headlessui/react'
+import { Combobox, Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
 
@@ -15,7 +15,6 @@ export default function Create({auth, permissions, roles}) {
     })
 
     const [selectedRoles, setSelectedRoles] = useState([])
-    const [query, setQuery] = useState('')
 
     const submit = (e) => {
         e.preventDefault();
@@ -37,13 +36,6 @@ export default function Create({auth, permissions, roles}) {
         });
     };
 
-    const filteredRoles =
-        query === ''
-            ? roles
-            : roles.filter((role) => {
-                return role.name.toLowerCase().includes(query.toLowerCase())
-            })
-
     return (
         <AuthenticatedLayout user={auth.user} permissions={permissions}>
             <Head title="Create user"/>
@@ -63,77 +55,55 @@ export default function Create({auth, permissions, roles}) {
                         onChange={e => setData('email', e.target.value)}
                     />
                     <div className="w-full max-w-md m-2">
-                        <Combobox value={selectedRoles} onChange={value => {
-                            setSelectedRoles(value)
-                            let roles = value.map(item => item.id)
-                            setData('roles', roles)
-                        }} multiple>
-                            <div className="relative mt-1">
-                                <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-primary text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                                    <Combobox.Input
-                                        className="w-full border-none py-2 pl-3 pr-10 text-sm bg-primary leading-5 focus:ring-0"
-                                        displayValue={(roles) =>
-                                            roles.map((role) => role.name).join(', ')
-                                        }
-                                        onChange={(event) => setQuery(event.target.value)}
-                                    />
-                                    <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                                        <ChevronUpDownIcon
-                                            className="h-5 w-5 text-gray-400"
-                                            aria-hidden="true"
-                                        />
-                                    </Combobox.Button>
-                                </div>
+                        <div className="relative mt-1">
+                            <Listbox value={selectedRoles} onChange={value => {
+                                setSelectedRoles(value)
+                                let roleIds = value.map(item => item.id)
+                                setData('roles', roleIds)
+                            }} multiple>
+                                <Listbox.Button className="w-full border-none btn pl-3 pr-10 text-sm bg-primary leading-5 focus:ring-0">
+                                    {selectedRoles.length === 0 && <h6>Role</h6>}
+                                    {selectedRoles.map(selectedRole => selectedRole.name).join(', ')}
+                                </Listbox.Button>
                                 <Transition
                                     as={Fragment}
                                     leave="transition ease-in duration-100"
                                     leaveFrom="opacity-100"
                                     leaveTo="opacity-0"
-                                    afterLeave={() => setQuery('')}
                                 >
-                                    <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-primary py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                                        {filteredRoles.length === 0 && query !== '' ? (
-                                            <div className="relative cursor-default select-none px-4 py-2">
-                                                Nothing found.
-                                            </div>
-                                        ) : (
-                                            filteredRoles.map((role) => (
-                                                <Combobox.Option
-                                                    key={role.id}
-                                                    className={({ active }) =>
-                                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                                            active ? 'bg-secondary' : ''
-                                                        }`
-                                                    }
-                                                    value={role}
-                                                >
-                                                    {({ selected, active }) => (
-                                                        <>
-                                                            <span
-                                                                className={`block truncate ${
-                                                                    selected ? 'font-medium' : 'font-normal'
-                                                                }`}
-                                                            >
-                                                              {role.name}
-                                                            </span>
-                                                            {selected ? (
-                                                                <span
-                                                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                                                        active ? '' : 'text-secondary'
-                                                                    }`}
-                                                                >
+                                    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-primary py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                                        {roles.map(role => (
+                                            <Listbox.Option
+                                                key={role.id}
+                                                className={({ active }) =>
+                                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                                        active ? 'bg-secondary' : ''
+                                                    }`
+                                                }
+                                                value={role}
+                                            >
+                                                {({ selected }) => (
+                                                    <>
+                                                      <span
+                                                          className={`block truncate ${
+                                                              selected ? 'font-medium' : 'font-normal'
+                                                          }`}
+                                                      >
+                                                        {role.name}
+                                                      </span>
+                                                        {selected ? (
+                                                            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                                                                 <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                              </span>
-                                                            ) : null}
-                                                        </>
-                                                    )}
-                                                </Combobox.Option>
-                                            ))
-                                        )}
-                                    </Combobox.Options>
+                                                            </span>
+                                                        ) : null}
+                                                    </>
+                                                )}
+                                            </Listbox.Option>
+                                        ))}
+                                    </Listbox.Options>
                                 </Transition>
-                            </div>
-                        </Combobox>
+                            </Listbox>
+                        </div>
                     </div>
                     <PrimaryButton className="mt-4 m-2" disabled={processing}>Add</PrimaryButton>
                 </form>
