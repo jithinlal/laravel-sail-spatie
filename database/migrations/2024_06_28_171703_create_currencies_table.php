5682d7044911ac26f5058ce0f692b149f0c8986f
jithinlal-gelato <jithinlal.pr@gelato.com>
2024-07-01 20:24:32 +0530

    chore: currency seeder

--- database/migrations/2024_06_28_171703_create_currencies_table.php
+++ database/migrations/2024_06_28_171703_create_currencies_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('currencies', function (Blueprint $table) {
             $table->id();
             $table->string('code');
+            $table->string('name');
             $table->timestamps();
         });
     }
